7817e4175da72ae2ea5cf0ea64ac13faf0a2c930
Maksims Orlovs <orlovs@student.chalmers.se>
2024-11-28 17:59:31 +0100

    #2 Add endpoint for booking a slot

--- server/api-gateway/src/index.ts
+++ server/api-gateway/src/index.ts
@@ -12,11 +12,14 @@ const mqttOptions:IClientOptions = {
 	password: "Ilike2makewalks",
 };
 
-const requestTopic = 'request/';
+const requestTopic = 'appointments/';
 const responseTopic = 'response/';
-const heartbeatTopic = 'heartbeat/topic';
+const heartbeatTopic = 'heartbeat/appointments'; // TODO: general topics
 
 app.use(cors());
+// Parse requests of content-type 'application/json'
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 const mqttClient = mqtt.connect("tls://0fc2e0e6e10649f790f059e77c606dfe.s1.eu.hivemq.cloud:8883", mqttOptions);
 
@@ -57,8 +60,20 @@ mqttClient.on('connect', () => {
 	})
 });
 
-app.use("/appointment", (req: Request, res: Response) => {
-	const requestMessage = 'Request message';
+/**
+ * Format: 
+ *   Endpoint: /appointment
+ * 	 Body: { doctorId: <ObjectId>, startTime: <Date> }
+ */
+// TODO: Auth
+app.post("/appointment", (req: Request, res: Response) => {
+	if (!req.body?.doctorId || !req.body?.startTime) {
+		res.status(400).send('Error: Invalid request');
+    console.log(req.body);
+		return;
+	}
+
+  const requestMessage = JSON.stringify( { doctorId: req.body.doctorId, startTime: req.body.startTime } );
 	const options:IClientPublishOptions = { qos: 2 }
 	const timeoutDuration = 3000;
 	let retries = 0;
@@ -66,50 +81,56 @@ app.use("/appointment", (req: Request, res: Response) => {
 	
 	console.log("\n")
 
-	const publishRequest = (serviceId: string) => {
-        console.log(`Published request: ${requestMessage} to service ${serviceId}`);
+  const publishRequest = (serviceId: string) => {
+    console.log(`Published request: ${requestMessage} to ${requestTopic + serviceId + '/book'}`);
 
-		const messageHandler = (topic: string, message: Buffer) => {
-			if (topic === responseTopic + serviceId) {
-				clearTimeout(timeout);
-				res.send(`Reply: ${message.toString()}`);
-				mqttClient.removeListener('message', messageHandler);
-				console.log(`Received response: ${message.toString()}\n`);
-			}
-		};
-
-		mqttClient.on('message', messageHandler);
-
-        mqttClient.publish(requestTopic + serviceId, requestMessage, options, (err) => {
-            if (err) return res.status(500).send('Failed to publish request message');
-        });
-
-		const timeout = setTimeout(() => {
-			console.log(`Request to service ${serviceId} timed out. Redirecting to another service...`);
-			mqttClient.removeListener('message', messageHandler);
-			retries++;
-			if (retries < maxRetries) {
-				const newServiceId = servicesList.getRoundRobinService()?.getServiceId();
-				if (newServiceId) {
-					publishRequest(newServiceId);
-				} else {
-					res.status(500).send('No available services to handle the request');
-					console.log('No available services to handle the request\n');
-				}
-			} else {
-				res.status(500).send('Request timed out after multiple retries');
-				console.log('Request timed out after multiple retries\n');
-			}
-		}, timeoutDuration);
+    const messageHandler = (topic: string, message: Buffer) => {
+      if (topic === responseTopic + serviceId) {
+        clearTimeout(timeout);
+        res.send(`Reply: ${message.toString()}`);
+        mqttClient.removeListener('message', messageHandler);
+        console.log(`Received response: ${message.toString()}\n`);
+      }
     };
 
+    mqttClient.on('message', messageHandler);
+
+    mqttClient.publish(requestTopic + serviceId + '/book', requestMessage, options, (err) => {
+      if (err) return res.status(500).send('Failed to publish request message');
+    });
+
+    const timeout = setTimeout(() => {
+      console.log(`Request to service ${serviceId} timed out. Redirecting to another service...`);
+      mqttClient.removeListener('message', messageHandler);
+      retries++;
+      if (retries < maxRetries) {
+        const newServiceId = servicesList.getRoundRobinService()?.getServiceId();
+        if (newServiceId) {
+          publishRequest(newServiceId);
+        } else {
+          res.status(500).send('No available services to handle the request');
+          console.log('No available services to handle the request\n');
+        }
+      } else {
+        res.status(500).send('Request timed out after multiple retries');
+        console.log('Request timed out after multiple retries\n');
+      }
+    }, timeoutDuration);
+  };
+
 	const initialServiceId = servicesList.getRoundRobinService()?.getServiceId();
-    if (initialServiceId) {
-        publishRequest(initialServiceId);
-    } else {
-        res.status(500).send('No available services to handle the request');
-        console.log('No available services to handle the request\n');
-    }
+  if (initialServiceId) {
+    //publishRequest(initialServiceId);
+
+    mqttClient.publish(requestTopic + initialServiceId + '/book', requestMessage, options, (err) => {
+      console.log(`Published request: ${requestMessage} to ${requestTopic + initialServiceId + '/book'}`);
+      if (err) return res.status(500).send('Failed to publish request message');
+      else return res.status(200).send('Request sent');
+    });
+  } else {
+    res.status(500).send('No available services to handle the request');
+    console.log('No available services to handle the request\n');
+  }
 });
 
 app.use('/', (req: Request, res: Response, next: NextFunction) => {res.send('API Gateway')});
