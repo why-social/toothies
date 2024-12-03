92ce7909871f62a73f132b8245cf5f0b7eee51c7
Maksims Orlovs <orlovs@student.chalmers.se>
2024-12-03 14:26:30 +0100

    #5 Implement slot booking

--- client/src/app/views/booking/booking.ts
+++ client/src/app/views/booking/booking.ts
@@ -1,20 +1,22 @@
+import { Router } from "@angular/router";
+
 import {
   ChangeDetectionStrategy,
   Component,
   inject,
   Injectable,
-} from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { Calendar } from '../../components/calendar/calendar';
-import { Slot } from '../../types/slots';
-import { Dialog } from './dialog/dialog';
-import { HttpClient } from '@angular/common/http';
-import { ActivatedRoute } from '@angular/router';
+} from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
+import { Calendar } from "../../components/calendar/calendar";
+import { Slot } from "../../types/slots";
+import { Dialog } from "./dialog/dialog";
+import { HttpClient } from "@angular/common/http";
+import { ActivatedRoute } from "@angular/router";
 
-@Injectable({ providedIn: 'root' })
+@Injectable({ providedIn: "root" })
 @Component({
-  templateUrl: './booking.html',
-  styleUrl: './booking.css',
+  templateUrl: "./booking.html",
+  styleUrl: "./booking.css",
   imports: [Calendar],
   // changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -22,6 +24,7 @@ export class Booking {
   readonly dialog = inject(MatDialog);
   private http = inject(HttpClient);
   private route = inject(ActivatedRoute);
+  private router = inject(Router);
 
   slots: Array<Slot> = [];
   private doctorId: string | null = null;
@@ -29,7 +32,7 @@ export class Booking {
 
   public ngOnInit() {
     this.route.paramMap.subscribe((params) => {
-      this.doctorId = params.get('id');
+      this.doctorId = params.get("id");
       if (this.doctorId) this.fetchSlots();
     });
   }
@@ -49,10 +52,9 @@ export class Booking {
                 isBooked: it.isBooked,
               }) as Slot,
           );
-          console.log('Fetched: ', this.slots);
         },
         error: (error) => {
-          console.error('Error fetching slots: ', error);
+          console.error("Error fetching slots: ", error);
         },
       });
   }
@@ -62,26 +64,40 @@ export class Booking {
 
     this.dialog
       .open(Dialog, {
-        width: '250px',
+        width: "250px",
         data: {
-          title: 'Confirm Booking',
+          title: "Confirm Booking",
           message: `Time: ${slot.startTime.toLocaleTimeString([], {
-            hour: '2-digit',
-            minute: '2-digit',
+            hour: "2-digit",
+            minute: "2-digit",
             hour12: false,
           })} - ${slot.endTime.toLocaleTimeString([], {
-            hour: '2-digit',
-            minute: '2-digit',
+            hour: "2-digit",
+            minute: "2-digit",
             hour12: false,
           })}`,
         },
-        enterAnimationDuration: '200ms',
-        exitAnimationDuration: '200ms',
+        enterAnimationDuration: "200ms",
+        exitAnimationDuration: "200ms",
       })
       .afterClosed()
       .subscribe((result) => {
-        if (result == 'success') {
-          console.log(slot);
+        if (result == "success") {
+          console.log("posting");
+          this.http
+            .post(`http://localhost:3000/appointments`, {
+              doctorId: this.doctorId,
+              startTime: slot.startTime,
+            })
+            .subscribe({
+              next: () => {
+                this.router.navigate(["/"]);
+                // this.fetchSlots();
+              },
+              error: (error) => {
+                console.error("Error fetching slots: ", error);
+              },
+            });
         }
 
         this.pendingSlot = null;
