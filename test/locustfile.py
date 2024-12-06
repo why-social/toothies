f237d8c55bfd3ba4174b15dc1a5410f4849f18ba
Maksims Orlovs <orlovs@student.chalmers.se>
2024-12-06 11:26:26 +0100

    #23 Update db connection logic

--- test/locustfile.py
+++ test/locustfile.py
@@ -2,46 +2,19 @@ import time
 from random import randint
 from datetime import datetime as date
 from datetime import timedelta as delta
-from locust import HttpUser, task, between
+from locust import HttpUser, task, between, events
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+from locust.runners import MasterRunner
 
 MIN_HOUR = 8
 MAX_HOUR = 20
 STEP_MINUTES = 10
-testDate = date(2026, 1, 1).replace(hour=MIN_HOUR)
 step = delta(minutes=STEP_MINUTES)
-
-class BookingTests(HttpUser):
-    wait_time = between(1, 5) # wait 1-5 secs between requests
-
-    @task
-    def create_booking(self):
-        self.client.post("/appointments",
-            json={'doctorId': str(self.stressDoctorId), 'startTime': str(testDate + delta(hours=randint(0, MAX_HOUR-MIN_HOUR)))}
-        )
-
-    @task
-    def cancel_booking(self):
-        self.client.delete("/appointments",
-            json={'doctorId': str(self.stressDoctorId), 'startTime': str(testDate + delta(hours=randint(0, MAX_HOUR-MIN_HOUR)))}
-        )
-
-    def on_start(self):
-        client = connect_db()
-        db = client['primary']
-        self.stressDoctorId = db["doctors"].insert_one({"name": "Stress Test"}).inserted_id
-        generate_slots(db, self.stressDoctorId)
-        time.sleep(1)
-        client.close()
-
-    def on_stop(self):
-        client = connect_db()
-        db = client['primary']
-        cleanup(db)
+test_date = date(2025, 1, 1).replace(hour=MIN_HOUR)
 
 def connect_db():
-    uri = "mongodb+srv://nodejs:54AAuvmMv1osNAOz@cluster0.wft9l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    uri = "mongodb+srv://locust:63Q8l2RKvr2iuMvn@cluster0.wft9l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     # Create a new client and connect to the server
     client = MongoClient(uri, server_api=ServerApi('1'))
     # Send a ping to confirm a successful connection
@@ -60,17 +33,49 @@ def cleanup(db):
         deleted_docs += db["doctors"].delete_one(doc).deleted_count
     print(f"Deleted {deleted_docs} doctors and {deleted_slots} slots")
 
-def generate_slots(db, id):
+def generate_slots(db, id, y, m, d):
+    test_date = date(y, m, d).replace(hour=MIN_HOUR)
     db["slots"].insert_many([
-        {"doctorId": id, "startTime": testDate + i*step, "endTime": testDate + (i+1)*step}
+        {"doctorId": id, "startTime": test_date + i*step, "endTime": test_date + (i+1)*step}
         for i in range(0, (MAX_HOUR-MIN_HOUR)*60//STEP_MINUTES)
     ])
+
+client = connect_db()
+db = client['primary']
+stressDoctorId = db["doctors"].insert_one({"name": "Stress Test"}).inserted_id
+generate_slots(db, stressDoctorId, test_date.year, test_date.month, test_date.day)
+time.sleep(1) # be nice to Atlas
+client.close()
+
+@events.test_stop.add_listener
+def on_test_stop(environment, **kwargs):
+    client = connect_db()
+    db = client['primary']
+    cleanup(db)
+
+class BookingTests(HttpUser):
+    wait_time = between(1, 5) # wait 1-5 secs between requests
+
+    @task
+    def create_booking(self):
+        self.client.post("/appointments",
+            json={'doctorId': str(stressDoctorId), 'startTime': str(test_date + delta(hours=randint(0, MAX_HOUR-MIN_HOUR)))}
+        )
+
+    @task
+    def cancel_booking(self):
+        self.client.delete("/appointments",
+            json={'doctorId': str(stressDoctorId), 'startTime': str(test_date + delta(hours=randint(0, MAX_HOUR-MIN_HOUR)))}
+        )
+
 
 if __name__ == '__main__':
     client = connect_db()
     db = client['primary']
-    stressDoctorId = db["doctors"].insert_one({"name": "Stress Test"}).inserted_id
-    generate_slots(db, stressDoctorId)
+    stressDoctorId = db["doctors"].insert_one({"name": "Sven Svensson"}).inserted_id
+    for i in range(1,30):
+        generate_slots(db, stressDoctorId, 2025, 1, i)
+
     time.sleep(1)
-    cleanup(db)
+    # cleanup(db)
     client.close()
