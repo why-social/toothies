f6eb9f69de0f779e22c6c59c51d594f92c4698bd
Razvan Albu <razvana@student.chalmers.se>
2025-01-08 23:59:45 +0200

    #35 Add large screen layouts

--- client/src/app/views/admin/admin.ts
+++ client/src/app/views/admin/admin.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   templateUrl: './admin.html',
   styleUrl: './admin.css',
+  imports: [MatButtonModule, MatInputModule],
 })
 export class Admin {}
